cecbeb31a76ec6eaad758d51eddefa70513f480e
Dan Fockler <dfockler13@gmail.com>
2018-07-07 08:31:34 -0700

    challenge 2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 extern crate base64;
 extern crate hex;
 
-use base64::{encode};
-use hex::{decode};
-
 fn main() {
+    challenge1();
+    challenge2();
+}
+
+fn challenge1() {
     let x = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let output = hex_to_base64(x);
+    
     println!("{:?}", output);
 }
 
+fn challenge2() {
+    let x = "1c0111001f010100061a024b53535009181c";
+    let y = "686974207468652062756c6c277320657965";
+
+    let output = xor(&hex_to_bytes(x), &hex_to_bytes(y));
+
+    println!("{:?}", output);
+}
+
+fn hex_to_bytes(input: &str) -> Vec<u8> {
+    hex::decode(input).unwrap()
+}
+
 fn hex_to_base64(input: &str) -> String {
-    encode(&decode(input).unwrap())
+    base64::encode(&hex_to_bytes(input))
+}
+
+fn xor(a: &Vec<u8>, b: &Vec<u8>) -> String {
+    let mut output: Vec<u8> = Vec::new();
+
+    for (a, b) in a.iter().zip(b) {
+        output.push(a ^ b);
+    }
+
+    hex::encode(&output)
 }
